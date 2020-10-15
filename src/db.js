7f27e8460340ff9783df4d46671937f8ee8f37be
udykas <udykas@gmail.com>
2020-10-15 16:32:21 -0400

    added graphql server, getting error

---
+++ src/db.js
@@ -0,0 +1,9 @@
+//This file connects ot the remote prisma db and gives is the ability to query it with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+})
