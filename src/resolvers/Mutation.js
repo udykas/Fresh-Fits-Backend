64799655cfb9a7f80af6396b796d590f699cf3ba
udykas <udykas@gmail.com>
2021-01-12 15:36:20 -0500

    formatting

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -66,14 +66,14 @@ const Mutations = {
   },
   async signin(parent, { email, password }, ctx, info) {
     // 1. check if there is a user with that email
-    const user = await ctx.db.query.user({where: { email: email }});
+    const user = await ctx.db.query.user({ where: { email } });
     if(!user) {
-      throw new Error(`No such user found for email ${email}`);
+      throw new Error(`No such user found for email: ${email}`);
     }
     // 2. check if password is correct
-    const vaild = await bcrypt.compare(password, user.password);
+    const valid = await bcrypt.compare(password, user.password);
     if(!valid) {
-      throw new Error("Invalid Password!")
+      throw new Error("Invalid Password!");
     }
     // 3. generate the JWT token
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -10,7 +10,7 @@ const Query = {
       return null;
     }
     return ctx.db.query.user({
-      where: {id: ctx.request.userId}
+      where: {id: ctx.request.userId},
     }, info);
   }
 };
